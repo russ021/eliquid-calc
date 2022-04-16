98cd3290b0b7721dd14ee5c5511128b77bb82ea4
russ021 <rbaird78@hotmail.com>
2022-04-16 09:21:39 -0400

    def again()

--- calculator.py
+++ calculator.py
@@ -30,5 +30,26 @@ Please type in the math operation you would like to complete:
     else:
         print('You have not typed a valid operator, please run the program again.')
 
+# Define again() function to ask user if they want to use the calculator again
+def again():
+
+    # Take input from user
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES of N for NO.
+''')
+
+    # If user types Y, run the calculate() function
+    if calc_again == 'Y':
+        calculate()
+
+    # If user types N, say good-bye to the user and end the program
+    elif calc_again == 'N':
+        print('See you later.')
+
+    # If user types another key, run the function again
+    else:
+        again()
+
 # Call calculate() outside of the function
 calculate()
